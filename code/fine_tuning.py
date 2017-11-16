e1571b87ec3efa9706e4755fa3d699e5ac8a17c9
MengTianjian <tim40@pitt.edu>
2017-11-16 18:48:16 -0500

    Add Fine tuning code

--- code/fine_tuning.py
+++ code/fine_tuning.py
@@ -25,29 +25,65 @@ from keras.callbacks import ReduceLROnPlateau, LearningRateScheduler
 # custom callbacks, not the original keras one
 from utils.callbacks import TensorBoard, SnapshotModelCheckpoint, ModelCheckpoint
 import utils.utils as utils
+from keras.layers import Lambda, BatchNormalization, Dense, Activation
 from model import *
+from keras.models import Model, Sequential
 
 
 # Load bottleneck features
-train_data = np.load(open('bottleneck_features_train.npy'))
-val_data = np.load(open('bottleneck_features_val.npy'))
+#train_data = np.load(open('bottleneck_features_train.npy'))
+train_data = np.zeros((10000,2048))
+#val_data = np.load(open('bottleneck_features_val.npy'))
+val_data = np.zeros((5000,2048))
 
 # Load labels
-train_label = np.zeros((len(batch_x), self.__num_label), dtype=K.floatx())
-train_label_level1 = np.zeros((len(batch_x), self.__num_label_level1), dtype=K.floatx())
-train_label_level2 = np.zeros((len(batch_x), self.__num_label_level2), dtype=K.floatx())
+train_label = np.zeros((10000,1))#(len(batch_x), self.__num_label), dtype=K.floatx())
+#train_label_level1 = np.zeros((len(batch_x), self.__num_label_level1), dtype=K.floatx())
+#train_label_level2 = np.zeros((len(batch_x), self.__num_label_level2), dtype=K.floatx())
 
-val_label = np.zeros((len(batch_x), self.__num_label), dtype=K.floatx())
-val_label_level1 = np.zeros((len(batch_x), self.__num_label_level1), dtype=K.floatx())
-val_label_level2 = np.zeros((len(batch_x), self.__num_label_level2), dtype=K.floatx())
+val_label = np.zeros((5000,1))#(len(batch_x), self.__num_label), dtype=K.floatx())
+#val_label_level1 = np.zeros((len(batch_x), self.__num_label_level1), dtype=K.floatx())
+#val_label_level2 = np.zeros((len(batch_x), self.__num_label_level2), dtype=K.floatx())
 
+def model(shape,num_classes):#, num_filters):
+    #base_model = Xception(include_top=False, weights=None, input_shape=(180,180,3),classes=num_classes[2])
+    model = Sequential()
+    #output = []
+    #input = model.input
+    '''for i in range(2):
+        b = SeparableConv2D(num_filters, (3,3), padding='same', use_bias=False, name='b'+str(i+1)+'_sepconv1')(input)
+        b = BatchNormalization(name='b'+str(i+1)+'sepconv1_bn')(b)
+        b = Activation(actvation, name='b'+str(i+1)+'sepconv1_act')(b)
+        b = SeparableConv2D(num_filters, (3,3), padding='same', use_bias=False, name='b'+str(i+1)+'_sepconv2')(b)
+        b = BatchNormalization(name='b'+str(i+1)+'sepconv2_bn')(b)
+        b = Activation(actvation, name='b'+str(i+1)+'sepconv2_act')(b)
+        output.append(Dense(num_classes[i], name='b'+str(i+1))(b))'''
+    #x = Dense(128,input_shape=train_data.shape[1:])(input)
+    #x = BatchNormalization()(x)
+    #x = Activation("relu")(x)
+    #output.append(Dense(num_classes[2], name="out")(x))
+    #output = Dense(num_classes[2], name="out")(x)
+    #model = Model(inputs=base_model.input, outputs=[output[0], output[1], output[2]])
+    #model = Model(inputs=model.input, outputs=output)
+    model.add(Dense(128,input_shape=shape))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Dense(num_classes[2]))
+    return model
 
+num_classes = [utils.num_classes, utils.num_class_level_one, utils.num_class_level_two]
 
+model = model(train_data.shape[1:],num_classes)#, 728)
 
-# Last several layers to be fune-tuned
-model = Sequential()
-model.add(Flatten(input_shape=train_data.shape[1:]))
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+initial_learning_rate = 0.001
+adam = Adam(lr=initial_learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
+model.compile(optimizer=adam,
+              loss=DARC1,
+              #loss_weight=[lw1, lw2, lw3],
+              metrics=["accuracy"],
+              )
+model.fit(train_data, train_label,
+          epochs=50,
+          batch_size=128,
+          validation_data=(val_data, val_label))
